cdee82a1431dc357c3405cfb9ae8f6679c235c0c
Dave Forgac <dave.forgac@complion.com>
2019-04-28 03:37:25 -0400

    handle status

--- tutorial_runner/cli.py
+++ tutorial_runner/cli.py
@@ -1,5 +1,7 @@
 import sys
 import click
+import pathlib
+import pytest
 
 from tutorial_runner.state import State
 
@@ -9,7 +11,8 @@ from tutorial_runner.state import State
 def tutorial(ctx):
     """Click tutorial runner."""
     ctx.ensure_object(dict)
-    ctx.obj['state'] = State()
+    ctx.obj["state"] = State()
+
 
 @tutorial.command()
 def hint():
@@ -19,7 +22,12 @@ def hint():
 
 @tutorial.command()
 @click.pass_obj
-@click.option("--config", type=click.File(), default='tutorial.toml', help="Tutorial configuration file.")
+@click.option(
+    "--config",
+    type=click.Path(exists=True, dir_okay=False),
+    default="tutorial.toml",
+    help="Tutorial configuration file.",
+)
 @click.option(
     "--reinitialize",
     "-r",
@@ -28,7 +36,7 @@ def hint():
 )
 def init(obj, config, reinitialize):
     """(Re-)Initialize the tutorial"""
-    state = obj['state']
+    state = obj["state"]
     if state.is_initialized() and reinitialize is not True:
         click.confirm(
             "Already initialized. Do you want to clear progress and start over?",
@@ -41,20 +49,54 @@ def init(obj, config, reinitialize):
 @tutorial.command()
 @click.pass_obj
 @click.argument("lesson-id", type=click.INT)
-@click.option("--part", "-p")
-def lesson(obj, lesson_id, part):
-    """Set the current lesson ID"""
-    state = obj['state']
-    if part is None:
-        part = state.get_current_part()
-    
+@click.option("--part-id", "-p", type=click.INT, help="Specify part ID (defaults to current)")
+def lesson(obj, lesson_id, part_id):
+    """Switch to a specific lesson"""
+    state = obj["state"]
+    if part_id is None:
+        part_id = state.get_current_part_id()
+    state.set_current_lesson(part_id, lesson_id)
+
 
 @tutorial.command()
 @click.pass_obj
 def status(obj):
     """Show the status of your progress."""
+    state = obj["state"]
+    lesson = state.get_current_lesson()
+    click.echo("Current lesson\n--------------\n")
+    click.echo("Part {:02d}, Lesson {:02d}: {}".format(lesson['part']['id'], lesson['id'], lesson['name']))
+    click.echo(" Tutorial base dir: {}".format(lesson['tutorial_dir']))
+    click.echo("       Working dir: {}".format(lesson['part']['dir']))
+    click.echo("      Working file: {}".format(lesson['part']['file']))
+    parts = state.list_parts()
+    click.echo("\nAll lessons\n-----------")
+    for part in parts:
+        click.echo("\n-- Part {id:02d} - {name} --".format(**part))
+        for lesson in part['lessons']:
+            lesson['_status'] = state.get_lesson_status(part['id'], lesson['id'])
+            click.echo("{id:02d} - {name:20} - {_status}".format(**lesson))
+
+def run_lesson(lesson_test_file):
+    result = pytest.main(['-vx', 'tutorial/{0}'.format(lesson_test_file)])
+    if result == 0:
+        return True
+    else:
+        return False
+
+@tutorial.command()
+@click.pass_obj
+def check(obj):
+    """Check your work for the current lesson."""
     state = obj['state']
-    click.echo("OK")
+    lesson = state.get_current_lesson()
+    test_path = str(pathlib.Path(lesson['tutorial_dir'], lesson['part']['dir'], 'tests',lesson['test']))
+    print(test_path)
+    result = pytest.main(['-vx', test_path])
+    if result == 0:
+        print("Success!")
+    else:
+        print("Failed :-(")
 
 
 if __name__ == "__main__":
--- tutorial_runner/state.py
+++ tutorial_runner/state.py
@@ -6,6 +6,7 @@ from pathlib import Path
 
 APP_NAME = "Tutorial Runner"
 
+
 class State:
     def __init__(self):
         self.app_dir = click.get_app_dir(APP_NAME)
@@ -16,25 +17,31 @@ class State:
             pytoml.dump(new_state, statefile)
 
     def load(self):
-        with open(self.state_file_path, mode="r", encoding="utf-8") as statefile:
-            return pytoml.load(statefile)
+        try:
+            with open(self.state_file_path, mode="r", encoding="utf-8") as statefile:
+                return pytoml.load(statefile)
+        except FileNotFoundError as e:
+            raise click.ClickException("Tutorial status file not found. You probably need to run `tutorial init`. \nDetails: {}".format(e))
 
-    def initialize(self, config):
-        config_data = pytoml.load(config)
+    def initialize(self, config_filename):
+        with open(config_filename) as configfile:
+            config_data = pytoml.load(configfile)
+        tutorial_dir = str(Path(config_filename).resolve().parent)
         if not Path(self.app_dir).exists():
             Path(self.app_dir).mkdir(parents=True, exist_ok=True)
         default_state = {
-            "name": config_data.get('name'),
-            "parts": config_data.get('parts'),
-            "current": {'part': 1, 'lesson': 1},
-            "progress": {},
+            "name": config_data.get("name"),
+            "parts": config_data.get("parts"),
+            "tutorial_dir": tutorial_dir,
+            "current": {"part": 1, "lesson": 1},
+            "progress": {"1.1": "in-progress"},
         }
         self.save(default_state)
 
     def is_initialized(self):
         try:
             state = self.load()
-        except FileNotFoundError:
+        except click.ClickException:
             return False
         except:
             raise
@@ -42,17 +49,50 @@ class State:
             return True
         else:
             return False
-    
+
     def get_lesson_status(self, part, lesson):
+        progress_key = "{}.{}".format(part, lesson)
         state = self.load()
+        return state["progress"].get(progress_key, "incomplete")
 
-
-    def get_current_part(self):
+    def set_lesson_status(self, part_id, lesson_id, status):
+        progress_key = "{}.{}".format(part_id, lesson_id)
         state = self.load()
-        return state.get('current', {}).get('part')
+        state["progress"][progress_key] = status
+        self.save(state)
+
+    def get_current_part_id(self):
+        state = self.load()
+        return state.get("current", {}).get("part")
+
+    def get_current_lesson_id(self):
+        state = self.load()
+        return state.get("current", {}).get("lesson")
 
     def get_current_lesson(self):
+        part_id = self.get_current_part_id()
+        lesson_id = self.get_current_lesson_id()
         state = self.load()
-        return state.get('current', {}).get('part')
-
+        part = [p for p in state['parts'] if p['id'] == part_id][0]
+        lesson = [l for l in part['lessons'] if l['id'] == lesson_id][0]
+        lesson['part'] = part
+        lesson['tutorial_dir'] = state['tutorial_dir']
+        return lesson
     
+    def set_current_lesson(self, part_id, lesson_id):
+        state = self.load()
+        try:
+            part = [p for p in state['parts'] if p['id'] == part_id][0]
+        except IndexError:
+            raise click.ClickException('{} is not a valid part ID. See `tutorial status` for a list of parts and lessons.'.format(part_id))
+        try:
+            lesson = [l for l in part['lessons'] if l['id'] == lesson_id][0]
+        except IndexError:
+            raise click.ClickException('{} is not a valid lesson ID for part {:02d}. See `tutorial status` for a list of parts and lessons.'.format(lesson_id, part_id))
+        state['current'] = {"part": part_id, "lesson": lesson_id}
+        self.save(state)
+        self.set_lesson_status(part_id, lesson_id, 'in-progress')
+         
+    def list_parts(self):
+        state = self.load()
+        return state['parts']
