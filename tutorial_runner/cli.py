e1cd4415d3cb9b5f587516107118b2e2bab75ea6
Dave Forgac <dave.forgac@complion.com>
2019-04-28 01:55:55 -0400

    make state a class

--- tutorial_runner/cli.py
+++ tutorial_runner/cli.py
@@ -1,13 +1,15 @@
 import sys
 import click
 
-from tutorial_runner import state
+from tutorial_runner.state import State
 
 
 @click.group()
-def tutorial(args=None):
+@click.pass_context
+def tutorial(ctx):
     """Click tutorial runner."""
-
+    ctx.ensure_object(dict)
+    ctx.obj['state'] = State()
 
 @tutorial.command()
 def hint():
@@ -16,33 +18,43 @@ def hint():
 
 
 @tutorial.command()
+@click.pass_obj
+@click.option("--config", type=click.File(), default='tutorial.toml', help="Tutorial configuration file.")
 @click.option(
     "--reinitialize",
     "-r",
     is_flag=True,
     help="Clear progress and reinitialize tutorial.",
 )
-def init(reinitialize):
+def init(obj, config, reinitialize):
     """(Re-)Initialize the tutorial"""
+    state = obj['state']
     if state.is_initialized() and reinitialize is not True:
         click.confirm(
             "Already initialized. Do you want to clear progress and start over?",
             abort=True,
         )
-    state.initialize()
+    state.initialize(config)
     click.echo("Tutorial initialized! Time to start your first lesson!")
 
 
 @tutorial.command()
-def status():
-    """Show the status of your progress."""
-    click.echo("OK")
-
+@click.pass_obj
+@click.argument("lesson-id", type=click.INT)
+@click.option("--part", "-p")
+def lesson(obj, lesson_id, part):
+    """Set the current lesson ID"""
+    state = obj['state']
+    if part is None:
+        part = state.get_current_part()
+    
 
 @tutorial.command()
-def verify():
-    """Verify that your environment is set up correctly."""
-    raise NotImplementedError()
+@click.pass_obj
+def status(obj):
+    """Show the status of your progress."""
+    state = obj['state']
+    click.echo("OK")
 
 
 if __name__ == "__main__":
--- tutorial_runner/state.py
+++ tutorial_runner/state.py
@@ -5,38 +5,54 @@ from codecs import open
 from pathlib import Path
 
 APP_NAME = "Tutorial Runner"
-APP_DIR = click.get_app_dir(APP_NAME)
+
+class State:
+    def __init__(self):
+        self.app_dir = click.get_app_dir(APP_NAME)
+        self.state_file_path = Path(self.app_dir, "tutorial-state.toml")
+
+    def save(self, new_state):
+        with open(self.state_file_path, mode="w", encoding="utf-8") as statefile:
+            pytoml.dump(new_state, statefile)
+
+    def load(self):
+        with open(self.state_file_path, mode="r", encoding="utf-8") as statefile:
+            return pytoml.load(statefile)
+
+    def initialize(self, config):
+        config_data = pytoml.load(config)
+        if not Path(self.app_dir).exists():
+            Path(self.app_dir).mkdir(parents=True, exist_ok=True)
+        default_state = {
+            "name": config_data.get('name'),
+            "parts": config_data.get('parts'),
+            "current": {'part': 1, 'lesson': 1},
+            "progress": {},
+        }
+        self.save(default_state)
+
+    def is_initialized(self):
+        try:
+            state = self.load()
+        except FileNotFoundError:
+            return False
+        except:
+            raise
+        if "parts" in state:
+            return True
+        else:
+            return False
+    
+    def get_lesson_status(self, part, lesson):
+        state = self.load()
 
 
-def state_file_path():
-    return Path(APP_DIR, "tutorial-state.toml")
+    def get_current_part(self):
+        state = self.load()
+        return state.get('current', {}).get('part')
 
+    def get_current_lesson(self):
+        state = self.load()
+        return state.get('current', {}).get('part')
 
-def save(new_state):
-    with open(state_file_path(), mode="w", encoding="utf-8") as statefile:
-        pytoml.dump(new_state, statefile)
-
-
-def load():
-    with open(state_file_path(), mode="r", encoding="utf-8") as statefile:
-        return pytoml.load(statefile)
-
-
-def initialize():
-    if not Path(APP_DIR).exists():
-        Path(APP_DIR).mkdir(parents=True, exist_ok=True)
-    default_state = {"lessons": {}}
-    save(default_state)
-
-
-def is_initialized():
-    try:
-        state = load()
-    except FileNotFoundError:
-        return False
-    except:
-        raise
-    if "lessons" in state:
-        return True
-    else:
-        return False
+    
